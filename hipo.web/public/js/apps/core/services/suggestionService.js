8b2c4337d5d6ad4fa4a0eb80847e78392fbd0a06
Martin Rabaglia <tinchoforever@gmail.com>
2013-07-14 11:51:13 -0300

    Conectando con hipo api

--- hipo.web/public/js/apps/core/services/suggestionService.js
+++ hipo.web/public/js/apps/core/services/suggestionService.js
@@ -5,14 +5,14 @@ var hipoApp = angular.module('hipoApp.services');
 hipoApp.service('suggestionService', function($http,profile) {
     return {
         getFor: function(key,callback){
-            var url = "/api/v2/suggestions/" + key;
+            var url = "/api/v1/start/" + key;
             var userProfile = profile.asSuggestionParameter();
             $http.post(url,userProfile).success(function(data) {
                  callback(data);
             });
         },
         get: function(callback){
-            var url = "/api/v2/suggestions";
+            var url = "/api/v1/start";
             var userProfile = profile.asSuggestionParameter();
             console.log(userProfile);
             $http.post(url,userProfile).success(function(data) {
