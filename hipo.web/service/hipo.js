07a453da2c5781e532dd71a58928e618e8525437
Lucas Rudi <rudilucas@gmail.com>
2013-07-14 13:47:18 -0300

    merge

--- hipo.web/service/hipo.js
+++ hipo.web/service/hipo.js
@@ -54,8 +54,37 @@ exports.match = function(req, res) {
 
 
 
+
     //TODO: PARSE PARAMS FOR CURRENT ACTIVITY
-    var parameters = mocks.params;
+    var parameters = [];
+    parameters.push({
+        name: "temperatura",
+        parsed: ["tem", "pera", "tura"],
+        value: Math.ceil(profile.weather.temp),
+        uom: {name: "grados", symbol: "˚"}
+    });
+
+    parameters.push({
+        name: "tabla marea",
+        parsed: ["tabla", "mar", "eas"],
+           value: Math.ceil((Math.random()*10)+1),
+        uom: {name: "metros", symbol: "m"}
+    });
+
+    parameters.push({
+        name: "bandera mar",
+        parsed: ["ban", "dera", "mar"],
+        value: "A",
+        uom: {name: "bandera", symbol: "B"}
+    });
+
+    parameters.push({
+        name: "medusas",
+        parsed: ["me", "du", "sas"],
+         value: Math.ceil((Math.random()*100)+1),
+        uom: {name: "porcentaje", symbol: "%"}
+    });
+       
 
     var activitySuggestion = {
         name: current.place,
