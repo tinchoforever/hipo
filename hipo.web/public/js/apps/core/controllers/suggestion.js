9b99e316ba7da9bbd1196bb4bdbb9dc39a030cbc
Martin Rabaglia <tinchoforever@gmail.com>
2013-07-14 14:42:31 -0300

    numerito

--- hipo.web/public/js/apps/core/controllers/suggestion.js
+++ hipo.web/public/js/apps/core/controllers/suggestion.js
@@ -1,7 +1,7 @@
 'use strict';
 var hipoApp = angular.module('hipoApp.controllers');
 
-hipoApp.controller('suggestionController', function ($scope,wizard,profile,suggestionService) {
+hipoApp.controller('suggestionController', function ($scope,wizard,profile,suggestionService, placesService) {
     $scope.ret = true;
     $scope.condition = "icon-" + "light-up";
 
@@ -12,6 +12,15 @@ hipoApp.controller('suggestionController', function ($scope,wizard,profile,sugge
         $scope.numberDay = decimos(moment().date());
         $scope.month=  moment().format('MMMM');
         $scope.condition = "icon-" + "water";
+        placesService.getAll(function(data){
+          for (var i = 0;i <data.length; i++) {
+                var point = data[i];
+                point.map = "http://staticmap.openstreetmap.de/staticmap.php?center=" + point.lat + ',' +point.lng + "&zoom=20&size=300x200&maptype=mapnik&markers="+ point.lat + ',' +point.lng +",lightblue1";
+
+            };
+            console.log(data[0]);
+            $scope.points = data;
+        });
     };
     //Si hay actividad, la mando
     if (wizard.currentActivity !== ""){
