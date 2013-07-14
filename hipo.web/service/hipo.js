a71dca0086854dc4a1986464668acff9d2c5c4f9
Lucas Rudi <rudilucas@gmail.com>
2013-07-14 13:13:25 -0300

    test still failing the async call

--- hipo.web/service/hipo.js
+++ hipo.web/service/hipo.js
@@ -2,25 +2,47 @@ var beach       = require('./matchers/beach')
   , pedestrian  = require('./matchers/pedestrian')
   , surf        = require('./matchers/surf')
   , array       = require ('array-extended')
-  , mocks       = require('../datasets/mocks.js');
+  , mocks       = require('../datasets/mocks.js')
+  , curly       = require('curly')
+  , async       = require('async');
+
 
 var matchers = [beach, pedestrian, surf];
+var weatherResult;
 exports.match = function(req, res) {
 
 	var user = req.body;
-    console.log(user.name);
-    console.log(user.tags);
-    console.log(user.mobility);
-    console.log(user.date);
+  console.log(user.name);
+  console.log(user.tags);
+  console.log(user.mobility);
+  console.log(user.date);
 
 
-    var profile = {};
-  	profile.user = user;
+  var profile = {};
+	profile.user = user;
 
-    profile.month = 11;
+  profile.month = 11;
 
-    //TODO: Get Weather
-	profile.weather = {};
+  //TODO: Get Weather
+
+  profile.weather = {};
+  
+  
+  async.series([function(callback) {
+      curly.get('https://api.forecast.io/forecast/3dc9db43b756c31a297a159fdb38b4d3/-37.9798584,-57.5897', function(err, response, body){
+        weatherResult = response.body.currently;
+        console.log("weatherResultweatherResultweatherResult", weatherResult);
+        callback();
+      });
+    }], function(err) { 
+      if (err) return next(err);
+      //Here locals will be populated with 'user' and 'posts'
+      console.log("weatherResult", weatherResult);
+  });
+
+  console.log("weatherResult2", weatherResult);
+  
+	
 	profile.weather.condition = 'rain';
 	profile.weather.wind = {};
 	profile.weather.temp = 13;
@@ -53,4 +75,8 @@ exports.match = function(req, res) {
     };
 
     res.json(activitySuggestion, 200);
+};
+
+var getCurrentWeather = function(callback) {
+  
 };
