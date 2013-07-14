af92a1adf0aad9d7a56f8aec5412717f3c6769ca
Lucas Rudi <rudilucas@gmail.com>
2013-07-13 23:08:51 -0300

    add rule engine for beach and started for pedestrian.

--- hipo.web/service/matchers/pedestrian.js
+++ hipo.web/service/matchers/pedestrian.js
@@ -1,3 +1,58 @@
+
 exports.match = function(profile, callback){
+	var staticInfo = {};
+	staticInfo.percentMatch = 0.9;
+    
+	var matchingrule = [
+	                    {'condition' : 'wind', 'less': 35},
+	                    {'condition' : 'wind', 'greater': 25},
+	                    {'condition' : 'monthOfYear', 'less' : 3},
+	                    {'condition' : 'temp', 'greater' : 15}
+	                   ];
+	var matching = 1;
+	for (currentRule in matchingrule) {
+		var rule = matchingrule[currentRule];
+		if (! rule.condition) {
+			continue;
+		}
+		if (rule.condition === 'wind') {
+			var compare = profile.weather.wind.speed;
+		}
+		if (rule.condition === 'condition') {
+			var compare = profile.weather.condition;
+		}
+		if (rule.condition === 'monthOfYear') {
+			var compare = profile.month;
+		}
+		if (rule.condition === 'temp') {
+			var compare = profile.weather.temp;
+		}
+
+		//comparator
+		if (rule.less) {
+			if (rule.less < compare) {
+				matching *= staticInfo.percentMatch;
+			}
+		}
+		if (rule.greater) {
+			if (rule.greater > compare) {				
+				matching *= staticInfo.percentMatch;
+			}
+		}
+		if (rule.not) {
+			if (rule.condition === compare) {
+				matching *= (staticInfo.percentMatch * 3);
+			}
+		}
+		if (rule.is) {
+			if (rule.condition !== compare) {
+				matching *= (staticInfo.percentMatch * 3);
+			}
+		}
+	}
+
+	console.log('adding walking places');
+	var suggestions = [{matching: matching, place : 'walk'}];
+	callback(suggestions);
 
 };
--- hipo.web/service/matchers/surf.js
+++ hipo.web/service/matchers/surf.js
@@ -1,3 +1,10 @@
 exports.match = function(profile, callback){
+	var matchingrule = [
+	                    {'condition' : 'wind', 'greater' : 5, 'less': 30},
+	                    {'condition' : 'dateOfYear', 'greater' : 20, 'less' : 10},
+	                   ];
+	console.log('adding surf places');
+	var suggestions = [{matching: 5, place : 'surf'}];
+	callback(suggestions);
 
 };
